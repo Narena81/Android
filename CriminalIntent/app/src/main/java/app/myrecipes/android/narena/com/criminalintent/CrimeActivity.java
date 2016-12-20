b0e3996889d56f1cf7d06f9047272c875e33dae7
Narena <narena@ibrisimovic.com>
2016-12-20 14:59:06 +1100

    create three class

--- CriminalIntent/app/src/main/java/app/myrecipes/android/narena/com/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/app/myrecipes/android/narena/com/criminalintent/CrimeActivity.java
@@ -1,14 +1,12 @@
 package app.myrecipes.android.narena.com.criminalintent;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -16,6 +14,18 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+        //Get fragment menager
+       FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+/*This code  creates and commit a fragments. Fragment transactions are used to add, remove, attach, detach,
+*or replace fragmens in the fragment list.
+
+*/
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
 
 
 
