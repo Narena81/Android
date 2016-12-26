df78005c42158dec9b4b7bd1e6e04c1983a35909
Narena <narena@ibrisimovic.com>
2016-12-26 13:39:10 +1100

    create Listener on RecyclerView

--- RecyclerView2/app/src/main/java/app/recyclerview/android/narena/com/recyclerview/MainActivity.java
+++ RecyclerView2/app/src/main/java/app/recyclerview/android/narena/com/recyclerview/MainActivity.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private List<Movie> movieList = new ArrayList<>();
     private RecyclerView recyclerView;
     private MoviesAdapter mAdapter;
+    int RecyclerViewItemPosition ;
+    View child;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +35,54 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mAdapter = new MoviesAdapter(movieList);
+        recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
 
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+            GestureDetector gestureDetector = new GestureDetector(MainActivity.this, new GestureDetector.SimpleOnGestureListener() {
+
+                @Override public boolean onSingleTapUp(MotionEvent motionEvent) {
+
+                    return true;
+                }
+
+            });
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+            public boolean onInterceptTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
 
+                child = Recyclerview.findChildViewUnder(motionEvent.getX(), motionEvent.getY());
+                System.out.println(Recyclerview.getChildAdapterPosition(child));
+                if (child != null && gestureDetector.onTouchEvent(motionEvent)) {
+                    
+
+                    Toast.makeText(MainActivity.this, String.valueOf(Recyclerview.getChildAdapterPosition(child)), Toast.LENGTH_LONG).show();
+
+
+                }
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView Recyclerview, MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+
+
+        });
         prepareMovieData();
-    }
+        }
+
+
 
     private void prepareMovieData() {
         Movie movie = new Movie("Mad Max: Fury Road", "Action & Adventure", "2015");
